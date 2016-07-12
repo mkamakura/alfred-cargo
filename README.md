0247350c4e9f62a0ae119082afe5a339f66f4c3c
mkamakura <kamakura.masaya@gmail.com>
2016-07-12 19:23:35 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# alfred-cargo
+
+> [Alfred 3](https://www.alfredapp.com) workflow to search for cargo packages with [crates.io](https://crates.io/)
+
+<img src="screenshot.png">
+
+
+## Install
+
+[Install the workflow.](http://www.packal.org/workflow/cargo)
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `cargo`, <kbd>Enter</kbd>, and your query, to search for packages.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const got = require('got');
+
+got('https://crates.io/api/v1/crates', {
+  json: true,
+  query: {
+    page: 1,
+    per_page: 10,
+    q: process.argv[2],
+  }
+})
+  .then(res => {
+    const items = res.body.crates.filter(x => x.name.length > 1)
+        .map(module => {
+          return {
+            title: module.name,
+            subtitle: module.description,
+            arg: `https://crates.io/crates/${module.name}`,
+            mods: {
+              alt: {
+                arg: `https://crates.io/crates/${module.name}`,
+                  subtitle: 'Open the cargo page'
+                }
+            },
+            quicklookurl: `https://crates.io/crates/${module.name}`
+          };
+        });
+    console.log(JSON.stringify({items}));
+  })
+  .catch(err => {
+	  console.log(JSON.stringify({
+		  items: [{
+		  	title: err.name,
+			  subtitle: err.message,
+			  valid: false,
+			  text: {
+			  	copy: err.stack
+			  }
+		  }]
+	  }));
+  });
